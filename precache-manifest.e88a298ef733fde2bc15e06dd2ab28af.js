bdd8f29f2563072e2c6566a502cbf1fd63c49740
Pedro Dias <pedro.r.dias.lec_a@msft.cesae.pt>
2023-06-21 10:39:13 +0100

    Updates

---
+++ precache-manifest.e88a298ef733fde2bc15e06dd2ab28af.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "3b516530148407034f24",
+    "url": "/color-palette/static/css/main.565f6f68.chunk.css"
+  },
+  {
+    "revision": "3b516530148407034f24",
+    "url": "/color-palette/static/js/main.b7f51d77.chunk.js"
+  },
+  {
+    "revision": "5d67c3cd455a1d0948f4",
+    "url": "/color-palette/static/js/runtime~main.56200f79.js"
+  },
+  {
+    "revision": "98dc0350c87a513300f5",
+    "url": "/color-palette/static/css/2.4bcee6a6.chunk.css"
+  },
+  {
+    "revision": "98dc0350c87a513300f5",
+    "url": "/color-palette/static/js/2.351a7433.chunk.js"
+  },
+  {
+    "revision": "77428cd498bf82179ff99fdb246b2494",
+    "url": "/color-palette/static/media/bg.77428cd4.svg"
+  },
+  {
+    "revision": "b035717419d6933e6ba5a85a6f9745de",
+    "url": "/color-palette/index.html"
+  }
+];
